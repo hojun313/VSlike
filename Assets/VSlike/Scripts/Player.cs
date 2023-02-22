db82ce491092e130468baa5a49bb6c3a00fcadb2
hojun313 <hojun313@naver.com>
2023-02-22 15:39:49 +0900

    플레이어 움직임 뉴

--- Assets/VSlike/Scripts/Player.cs
+++ Assets/VSlike/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -17,15 +18,12 @@ public class Player : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
-    }
-
     void FixedUpdate() {
         Vector2 nextVec = inputVec.normalized * speed * Time.deltaTime;
         rb.MovePosition(rb.position + nextVec);
+    }
+
+    void OnMove(InputValue value) {
+        inputVec = value.Get<Vector2>();
     }
 }
