986b5c8f1fc05a5b51194ddd71d942d6c812cc35
hojun313 <hojun313@naver.com>
2023-02-23 08:59:35 +0900

    플레이어 애니메이션

--- Assets/VSlike/Scripts/Player.cs
+++ Assets/VSlike/Scripts/Player.cs
@@ -7,10 +7,14 @@ public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
     public float speed;
+    Animator anim;
 
     Rigidbody2D rb;
+    SpriteRenderer sr;
     void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
     // Start is called before the first frame update
     void Start()
@@ -25,5 +29,13 @@ public class Player : MonoBehaviour
 
     void OnMove(InputValue value) {
         inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate() {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0) {
+            sr.flipX = inputVec.x < 0;
+        }
     }
 }
