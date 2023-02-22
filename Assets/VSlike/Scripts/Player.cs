e1407ca17bde3bb8be766e0a82befdb7631d1e14
hojun313 <hojun313@naver.com>
2023-02-22 10:50:15 +0900

    캐릭터 움직임

---
+++ Assets/VSlike/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rb;
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() {
+        Vector2 nextVec = inputVec.normalized * speed * Time.deltaTime;
+        rb.MovePosition(rb.position + nextVec);
+    }
+}
