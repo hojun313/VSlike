2130cb2f54c8a2e4266160eda378b08741274d06
hojun313 <hojun313@naver.com>
2023-02-28 13:04:27 +0900

    레벨 시스템 적용

--- Assets/VSlike/Scripts/Spawner.cs
+++ Assets/VSlike/Scripts/Spawner.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoints;
+    public SpawnData[] spawnData;
 
+    int level; 
     float timer;
 
     void Awake() {
@@ -15,15 +17,25 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 10f), spawnData.Length - 1);
 
-        if (timer > 0.2f){
+        if (timer > (spawnData[level].spawnTime)){
             timer = 0;
             Spawn();
         }
     }
 
     void Spawn(){
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, 2));
+        GameObject enemy = GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoints[Random.Range(1, spawnPoints.Length)].position;
+        enemy.GetComponent<Enemy>().Init(spawnData[level]);
     }
+}
+
+[System.Serializable]
+public class SpawnData{
+    public int spriteType;
+    public float spawnTime;
+    public int health;
+    public float speed;
 }
